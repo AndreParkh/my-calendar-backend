67152bb2f3a01d31908198777dfdb3f05a775ff3
AndreParkh <andreybmx95@mail.ru>
2025-09-15 22:02:52 +0300

    added environment to cors

--- src/main/kotlin/io/github/andreparkh/config/CorsConfig.kt
+++ src/main/kotlin/io/github/andreparkh/config/CorsConfig.kt
@@ -1,11 +1,15 @@
 package io.github.andreparkh.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CorsConfig: WebMvcConfigurer {
+
+    @Value("\${frontend.url}")
+    private lateinit var frontendUrl: String
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/auth/**")
@@ -23,7 +27,7 @@ class CorsConfig: WebMvcConfigurer {
             .maxAge(3600)
 
         registry.addMapping("/api/private/**")
-            .allowedOrigins("http://localhost:8080", "http://213.171.31.227:8080", "http://213.171.31.227")
+            .allowedOrigins(frontendUrl, "http://213.171.31.227")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("Content-Type", "Authorization")
             .allowCredentials(true)
